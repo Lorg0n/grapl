e7d627930324bf27afa8419c74ba35bedf0ea3c6
Lorg0n <lorgon.kv@gmail.com>
2026-01-02 23:24:57 +0300

    fix: deploy 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,57 @@
-import com.huskerdev.openglfx.plugins.utils.pom
-
 plugins {
-    alias(libs.plugins.pkmerboot.central.publisher) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.pkmerboot.central.publisher)
+    alias(libs.plugins.kotlin.jvm)
+    `java-library` 
+
     id("utils")
 }
 
 allprojects {
     repositories {
         mavenCentral()
-        mavenLocal()
     }
-    group = "com.huskerdev"
-    version = project.property("version") ?: "dev"
-}
-
-pom {
-    url = "https://github.com/husker-dev/"
-    licenses {
-        license {
-            name = "The Apache License, Version 2.0"
-            url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-        }
-    }
-    developers {
-        developer {
-            id = "husker-dev"
-            name = "Nikita Shtengauer"
-            email = "redfancoestar@gmail.com"
-        }
-    }
-    scm {
-        connection = "scm:git:git://github.com/husker-dev/grapl.git"
-        developerConnection = connection
-        url = this@pom.url
-    }
+    group = "com.github.lorg0n"
+    version = project.property("version") ?: "dev" 
 }
 
 publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("Grapl Fork")
+                description.set("A forked version of the Grapl library.")
+                url.set("https://github.com/Lorg0n/grapl")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Lorg0n")
+                        name.set("Your Name")
+                        email.set("your-email@example.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Lorg0n/grapl.git")
+                    developerConnection.set("scm:git:ssh://github.com/Lorg0n/grapl.git")
+                    url.set("https://github.com/Lorg0n/grapl/tree/main")
+                }
+            }
+        }
+    }
+
     repositories {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/Lorg0n/grapl")
             credentials {
                 username = System.getenv("GPR_USER") 
-                password = System.getenv("GPR_KEY")
+                password = System.getenv("GPR_KEY")  
             }
         }
     }
